323173c07f4212c51ce3b1202f35cb927b1d6c18
kiyodesu <kiyotomm99@gmail.com>
2024-10-31 01:36:54 +0900

    trigger commit

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -4,7 +4,7 @@ const NavItems = () => {
   const navItems = [
     { word: "Write", id: 1 },
     { word: "Sign in", id: 2, link: "signInPage" },
-    { word: "Get Started", id: 3 },
+    { word: "Get Started", id: 3, link: "blogsPage" },
   ];
   return (
     <Box
