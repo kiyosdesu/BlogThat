2cddf9e6e610eb53c806a7bac8787509016f7a23
kiyodesu <kiyotomm99@gmail.com>
2024-11-06 01:41:37 +0900

    wala lang

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,14 @@
+import { Box, Text, VStack } from "@chakra-ui/react";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <VStack width="100vw" height="100vh" alignItems="center " justify="center">
+      <Box alignSelf="start" marginLeft="20">
+        <Text fontSize="5xl">Kupals and Animals</Text>
+        <Text fontSize="3xl">Place to waste your time</Text>
+      </Box>
+    </VStack>
+  );
 };
 
 export default HomePage;
