63a4eeb300f0a2cff0aaacaaa55da84792bced37
Tom <kiyotomm99@gmail.com>
2024-11-06 21:00:56 +0900

    updated link

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -27,47 +27,45 @@ const BlogCard = ({
   const formattedDate = createdAt.toString();
   return (
     <>
-      <Link to="">
-        <VStack maxWidth="sm" overflow="hidden" padding="5" paddingY="10">
-          <Image
-            src={image}
-            width="98vw"
-            height="200px"
-            fit="cover"
-            overflow="hidden"
-          />
+      <VStack maxWidth="sm" overflow="hidden" padding="5" paddingY="10">
+        <Image
+          src={image}
+          width="98vw"
+          height="200px"
+          fit="cover"
+          overflow="hidden"
+        />
 
-          <HStack alignSelf="start" marginY="2">
-            <Image
-              src={userAvatar}
-              boxSize="30px"
-              fit="cover"
-              borderRadius="full"
-            />
-            <Text>{userName}</Text>
-          </HStack>
-          <VStack alignSelf="start">
-            <Text
-              fontSize="2xl"
-              fontWeight="bolder"
-              paddingY="2"
-              alignSelf="start"
-            >
-              {title}
-            </Text>
-            <Text fontWeight="extralight">{description}</Text>
-          </VStack>
-          <HStack alignSelf="start">
-            <Text fontSize="13px" fontWeight="normal">
-              {formattedDate}
-            </Text>
-            <FaRegThumbsUp />
-            <Text fontSize="12px">{likeCount}</Text>
-            <FaRegCommentAlt size="16px" />
-            <Text>{commentCount}</Text>
-          </HStack>
+        <HStack alignSelf="start" marginY="2">
+          <Image
+            src={userAvatar}
+            boxSize="30px"
+            fit="cover"
+            borderRadius="full"
+          />
+          <Text>{userName}</Text>
+        </HStack>
+        <VStack alignSelf="start">
+          <Text
+            fontSize="2xl"
+            fontWeight="bolder"
+            paddingY="2"
+            alignSelf="start"
+          >
+            {title}
+          </Text>
+          <Text fontWeight="extralight">{description}</Text>
         </VStack>
-      </Link>
+        <HStack alignSelf="start">
+          <Text fontSize="13px" fontWeight="normal">
+            {formattedDate}
+          </Text>
+          <FaRegThumbsUp />
+          <Text fontSize="12px">{likeCount}</Text>
+          <FaRegCommentAlt size="16px" />
+          <Text>{commentCount}</Text>
+        </HStack>
+      </VStack>
     </>
   );
 };
