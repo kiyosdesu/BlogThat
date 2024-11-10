ff83ebc3aafd678a16d4d41319b975de389e49ff
kiyodesu <kiyotomm99@gmail.com>
2024-11-11 00:58:04 +0900

    added skeleton for blogdetailapage

--- src/components/BlogDetailsSkeleton.tsx
+++ src/components/BlogDetailsSkeleton.tsx
@@ -1,18 +1,27 @@
-import { HStack, VStack } from "@chakra-ui/react";
-import {
-  Skeleton,
-  SkeletonCircle,
-  SkeletonText,
-} from "@/components/ui/skeleton";
+import { Box, HStack, VStack } from "@chakra-ui/react";
+import { Skeleton, SkeletonCircle, SkeletonText } from "./ui/skeleton";
 
 const BlogDetailsSkeleton = () => {
   return (
-    <VStack gap="6" maxW="xs">
-      <HStack width="full">
-        <SkeletonCircle size="10" />
-        <SkeletonText noOfLines={2} />
+    <VStack width="50vw" alignItems="start" justify="center" gap="7">
+      <Box width="10vw">
+        <SkeletonText noOfLines={1} />
+      </Box>
+      <Box width="30vw">
+        <SkeletonText noOfLines={1} height="10" />
+      </Box>
+      <Box width="35vw">
+        <SkeletonText noOfLines={1} height="5" />
+      </Box>
+      <HStack width="100%" gap="7">
+        <SkeletonCircle size="6vh" />
+        <VStack width="12vw">
+          <SkeletonText noOfLines={2} />
+        </VStack>
       </HStack>
-      <Skeleton height="200px" />
+      <SkeletonText noOfLines={2} />
+      <Skeleton height="40vh" width="50vw" />
+      <SkeletonText noOfLines={3} />
     </VStack>
   );
 };
