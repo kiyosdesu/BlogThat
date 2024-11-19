ef6abac72f9ec18fd611216fabdc9fea0515683f
Tom <kiyotomm99@gmail.com>
2024-11-19 13:38:34 +0900

    fix again

---
+++ src/context/SessionContext.tsx
@@ -0,0 +1,24 @@
+import useSession, { UseSession } from "@/components/hooks/useSession";
+import React, { createContext, useContext } from "react";
+
+const SessionContext = createContext<UseSession | null>(null);
+
+export const SessionProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const session = useSession();
+
+  // hi
+
+  return (
+    <SessionContext.Provider value={session}>
+      {children}
+    </SessionContext.Provider>
+  );
+};
+
+export const useSessionContext = () => {
+  return useContext(SessionContext);
+};
