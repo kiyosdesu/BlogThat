a16f2273a6c973101cfbfb07d34e54160abe7e5d
Tom <kiyotomm99@gmail.com>
2024-11-20 22:17:37 +0900

    fixed colormode icon

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,4 +1,4 @@
-import { Button, Text } from "@chakra-ui/react";
+import { Button, Text, Box } from "@chakra-ui/react";
 import { useColorMode } from "./ui/color-mode";
 import { CiLight } from "react-icons/ci";
 import { MdDarkMode } from "react-icons/md";
@@ -7,8 +7,16 @@ const ColorModeSwitch = () => {
   return (
     <>
       {/* <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} /> */}
-      <Button aria-label="toggle color mode" onClick={toggleColorMode}>
-        {colorMode === "dark" ? <CiLight /> : <MdDarkMode />}
+      <Button
+        aria-label="toggle color mode"
+        onClick={toggleColorMode}
+        variant="ghost"
+      >
+        {colorMode === "dark" ? (
+          <Box as={CiLight} boxSize="30px" />
+        ) : (
+          <Box as={MdDarkMode} boxSize="30px" />
+        )}
       </Button>
       <Text> </Text>
     </>
